34b4afa396843fb9deb55a39761ec158ee287230
AntonioKN <pedrosousarema@hotmail.com>
2024-09-23 20:39:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-from flask import render_template
-from flask import request
-
+from flask import Flask, render_template, request
 import pusher
-
 import mysql.connector
 import datetime
 import pytz
@@ -20,21 +15,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    con.close()
     return render_template("app.html")
-
 
 @app.route("/buscar")
 def buscar():
     if not con.is_connected():
         con.reconnect()
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM sensor_log")
-
+    cursor.execute("SELECT * FROM tst0_cursos_pagos")
+    
     registros = cursor.fetchall()
     con.close()
 
-    return registros
+    return {"data": registros}
 
 @app.route("/registrar", methods=["GET"])
 def registrar():
@@ -44,20 +37,29 @@ def registrar():
         con.reconnect()
     cursor = con.cursor()
 
-    sql = "INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora) VALUES (%s, %s, %s)"
-    val = (args["temperatura"], args["humedad"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
+    sql = "INSERT INTO tst0_cursos_pagos (Telefono, Archivo) VALUES (%s, %s)"
+    val = (args["telefono"], args["archivo"])
     cursor.execute(sql, val)
     
     con.commit()
     con.close()
 
     pusher_client = pusher.Pusher(
-        app_id = "1868455",
-        key = "613876ac427a3cc5a9f9",
-        secret = "6768ebc9cfd867046a84",
-        cluster = "us3",
+        app_id="1868455",
+        key="613876ac427a3cc5a9f9",
+        secret="6768ebc9cfd867046a84",
+        cluster="us3",
         ssl=True
     )
 
-    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", args)
-    return args
+    curso_pago = {
+        "Id_Curso_Pago": cursor.lastrowid,
+        "Telefono": args["telefono"],
+        "Archivo": args["archivo"]
+    }
+
+    pusher_client.trigger("canalCursosPagos", "registroCursoPago", curso_pago)
+    return curso_pago
+
+if __name__ == "__main__":
+    app.run(debug=True)
